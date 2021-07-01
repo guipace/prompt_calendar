fcead174b94a643b102561bb74f6f5fb9cfe2805
Guilherme Pace <pace.gui@gmail.com>
2021-07-01 14:30:01 -0500

    create components

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader />
+    <ContentFrame />
+    <TheFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+import ContentFrame from './components/ContentFrame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader,
+    ContentFrame,
+    TheFooter
   }
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ vue-app/src/components/ContentFrame.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="content-frame">
+    Content Frame
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ContentFrame',
+}
+</script>
+
+<style>
+#content-frame {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  flex-grow: 1;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
