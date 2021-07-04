a579cd09fd25f763ab6cc32506e090b6087ef8f2
Guilherme Pace <pace.gui@gmail.com>
2021-07-04 12:56:20 -0500

    create header, contentFrame and calendar components

---
+++ src/components/Calendar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import dateFns from 'date-fns';
+
+const Calendar = () => {
+
+  return (
+    <div>
+      <div>HEADER</div>
+      <div>Days</div>
+      <div>Cells</div>
+    </div>
+  )
+}
+
+export default Calendar;
---
+++ src/components/TheHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TheHeader = () => {
+  
+
+  return (
+    <h1>HEADER</h1>
+  )
+}
+
+export default TheHeader
